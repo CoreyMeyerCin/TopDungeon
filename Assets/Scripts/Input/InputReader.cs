24b45e1b1e398c9723267c4ecdd4454baf418836
DesrialBecker <beckerdl@miamioh.edu>
2022-07-26 15:05:15 -0400

    one step closer

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -53,5 +53,4 @@ public class InputReader: IInputReader<TileBase>
             return gridOfInputTiles;
         }
     }
-
 }
---
+++ Assets/Scripts/Patterns/PatternManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+
+namespace WaveFunctionCollapse
+{
+
+public class PatternManager
+{
+        Dictionary<int, PatternData> patternDataIndexDictionary;
+        Dictionary<int, PatternNeighbours> patternPossibleNeighboursDictionary;
+        int _patternSize = -1;
+        IFindNeighbourStrategy strategy;//obstruction of a strategy to find neighbours
+        
+        public PatternManager(int patternSize)
+        {
+            _patternSize = patternSize;
+        }
+
+        public void ProcessGrid<T>(ValuesManager<T> valueManager, bool equalWeights, string strategyName = null)
+        {
+            NeighbourStrategyFactory strategyFactory = new NeighbourStrategyFactory();
+            strategy = strategyFactory.CreateInstance(strategyName == null ? _patternSize + "" : strategyName);
+            CreatePatterns(valueManager, strategy, equalWeights);
+        }
+
+        private void CreatePatterns<T>(ValuesManager<T> valueManager, IFindNeighbourStrategy strategy, bool equalWeights)
+        {
+            var patternFinderResult = PatternFinder.GetPatternDataFromGrid(valueManager, _patternSize, equalWeights);
+            //////////////This part is just for testing//////////////////////////////////////////////////WHEN NOT TESTING COMMENT THIS OUT///////////////////////////////////////
+
+            StringBuilder builder = null;
+            List<string> list = new List<string>();
+            for (int row = 0; row < patternFinderResult.GetGridLengthY(); row++)
+            {
+                builder = new StringBuilder();
+                for (int col = 0; col < patternFinderResult.GetGridLengthX(); col++)
+                {
+                    builder.Append(patternFinderResult.GetIndexAt(col, row) + " ");
+                }
+                list.Add(builder.ToString());
+            }
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Debug.Log(item);
+            }
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            patternDataIndexDictionary = patternFinderResult.PatternIndexDictionary;
+            GetPatternNeighbours(patternFinderResult,strategy);
+        }
+
+        private void GetPatternNeighbours(PatternDataResults patternFinderResult, IFindNeighbourStrategy strategy)
+        {
+            patternPossibleNeighboursDictionary = PatternFinder.FindPossibleNeighboursForAllPatters(strategy, patternFinderResult);
+        }
+
+        public PatternData GetPatternDataFromIndex(int index)
+        {
+            return patternDataIndexDictionary[index];
+        }
+
+        public HashSet<int> GetPossibleNeighboursForPatternInDirection(int patternIndex, Direction dir)
+        {
+            return patternPossibleNeighboursDictionary[patternIndex].GetNeighboursInDirection(dir);
+        }
+
+        public float GetPatterFrequency(int index)
+        {
+            return GetPatternDataFromIndex(index).FrequencyRelative;
+        }
+
+        public float GetPatternFrequencyLof2(int index)
+        {
+            return GetPatternDataFromIndex(index).FrequencyRelativeLog2;
+        }
+
+        public int GetNumberOfPatterns()
+        {
+            return patternDataIndexDictionary.Count;
+        }
+    }
+}
