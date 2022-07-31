2de7c49bcba6e099601935d565b4ce5016012a23
Corey Meyer <72515852+CoreyMeyerCin@users.noreply.github.com>
2022-07-31 16:12:53 -0400

    Wfc cont3 (#135)
    * Generation works and takes appropriate perameters
    
    * FINISHED WFC!
    
    Co-authored-by: DesrialBecker <beckerdl@miamioh.edu>

--- Assets/Scripts/Core/WFCCore.cs
+++ Assets/Scripts/Core/WFCCore.cs
@@ -20,25 +20,31 @@ namespace WaveFunctionCollapse
 
         public int[][] CreateOutputGrid()
         {
+            //Debug.Log("Hit 0");
             int iteration = 0;
             while (iteration < this.maxIterations)
             {
+                //Debug.Log("Hit 1");
+
                 CoreSolver solver = new CoreSolver(this.outputGrid, this.patternManager);
                 int innerIteration = 100;
                 while (!solver.CheckForConflicts() && !solver.CheckIfSolved())
                 {
+                    //Debug.Log("Hit 2");
                     Vector2Int position = solver.GetLowestEntropyCell();
                     solver.CollapseCell(position);
                     solver.Propagate();
                     innerIteration--;
                     if (innerIteration <= 0)
                     {
+                       // Debug.Log("Hit 3");
                         Debug.Log("Propagation is taking too long");
                         return new int[0][];
                     }
                 }
                 if (solver.CheckForConflicts())
                 {
+                    //Debug.Log("Hit 4");
                     Debug.Log("\n Conflict occured. Iteration: " + iteration);
                     iteration++;
                     outputGrid.ResetAllPossibilities();
@@ -46,6 +52,7 @@ namespace WaveFunctionCollapse
                 }
                 else
                 {
+                    //Debug.Log("Hit 5");
                     Debug.Log("Solved on: " + iteration);
                     this.outputGrid.PrintResultsToConsol();
                     break;
@@ -53,8 +60,10 @@ namespace WaveFunctionCollapse
             }
             if (iteration >= this.maxIterations)
             {
+                //Debug.Log("Hit 6");
                 Debug.Log("Coulnd solve the tilemap");
             }
+            //Debug.Log("Hit 7");
             return outputGrid.GetSolvedOutputGrid();
         }
     }
