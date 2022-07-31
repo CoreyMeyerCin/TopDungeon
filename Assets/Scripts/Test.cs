93e153a3e0859ac469c1741c15799170972b2a38
DesrialBecker <beckerdl@miamioh.edu>
2022-07-31 16:09:06 -0400

    FINISHED WFC!

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,23 +3,65 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using WaveFunctionCollapse;
 
 public class Test : MonoBehaviour
 {
-    public Tilemap input;
+    public Tilemap inputTilemap;
+    public Tilemap outputTilemap;
+
+    public int patternSize;
+    public int maxIteration = 500;
+    public int outputWidth = 5;
+    public int outputHeight = 5;
+    public bool equalWeights = false;
+
+    ValuesManager<TileBase> valueManager;
+    WFCCore core;
+    PatternManager manager;
+    public TileMapOutput output;
+
     // Start is called before the first frame update
     public void Start()
     {
-        InputReader reader = new InputReader(input);
+        CreateWFC();
+
+    }
+
+    public void CreateWFC()
+    {
+        InputReader reader = new InputReader(inputTilemap);
         var grid = reader.ReadInputToGrid();
-        ValuesManager<TileBase> valueManager = new ValuesManager<TileBase>(grid);
-        PatternManager manager = new PatternManager(2);// this is where we change our pattern size
-        manager.ProcessGrid(valueManager, false);
-        WFCCore core = new WFCCore(5, 5, 500, manager);//this handels our demensions and iterations   (width, height, iterations)
+        valueManager = new ValuesManager<TileBase>(grid);
+        manager = new PatternManager(patternSize);// this is where we change our pattern size
+        manager.ProcessGrid(valueManager, equalWeights);
+        core = new WFCCore(5, 5, 500, manager);//this handels our demensions and iterations   (width, height, iterations)
+
+        
+
+    }
+
+    public void CreateTilemap()
+    {
+        //Debug.Log("Inside of CreateTilemap()");
+        output = new TileMapOutput(valueManager, outputTilemap);
+        Debug.Log($"TEST {output.valueManager}\n{outputTilemap}");
         var result = core.CreateOutputGrid();
+        output.CreateOutput(manager, result,outputWidth,outputHeight);
+    }
+
+    public void SaveTilemap()
+    {
+        if (output.OutputImage != null)
+        {
+            outputTilemap = output.OutputImage;
+            GameObject objectToSave = outputTilemap.gameObject;
+
+            PrefabUtility.SaveAsPrefabAsset(objectToSave, "Assets/Saved/output.prefab");
+        }
     }
 
     // Update is called once per frame
