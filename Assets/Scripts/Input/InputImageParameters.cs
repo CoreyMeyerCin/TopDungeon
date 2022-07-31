171f5f440cf19e2c6a62ea1d286b50b6cd420a2a
Corey Meyer <72515852+CoreyMeyerCin@users.noreply.github.com>
2022-07-31 16:54:22 -0400

    Bugfixes (#136)
    Co-authored-by: DesrialBecker <beckerdl@miamioh.edu>

--- Assets/Scripts/Input/InputImageParameters.cs
+++ Assets/Scripts/Input/InputImageParameters.cs
@@ -5,83 +5,73 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-namespace WaveFunctionCollapse { 
-public class InputImageParameters
+namespace WaveFunctionCollapse
 {
-        Vector2Int? bottonRightTileCoords = null;
+    public class InputImageParameters
+    {
+        Vector2Int? bottomRightTileCoords = null;
         Vector2Int? topLeftTileCoords = null;
         BoundsInt inputTileMapBounds;
-        TileBase[] inputTilemapsTileArray;
-        Queue<TileContainer> stackOfTiles = new Queue<TileContainer>();
+        TileBase[] inputTilemapTilesArray;
+        Queue<TileContainer> stackOftiles = new Queue<TileContainer>();
         private int width = 0, height = 0;
         private Tilemap inputTilemap;
 
-        public Queue<TileContainer> StackOfTiles { get => stackOfTiles; set => stackOfTiles = value; }
-        public int Width { get => width;}
-        public int Height { get => height;}
+        public Queue<TileContainer> StackOftiles { get => stackOftiles; set => stackOftiles = value; }
+        public int Height { get => height; }
+        public int Width { get => width; }
+
 
         public InputImageParameters(Tilemap inputTilemap)
         {
             this.inputTilemap = inputTilemap;
             this.inputTileMapBounds = this.inputTilemap.cellBounds;
-            this.inputTilemapsTileArray = this.inputTilemap.GetTilesBlock(this.inputTileMapBounds);
+            this.inputTilemapTilesArray = this.inputTilemap.GetTilesBlock(this.inputTileMapBounds);
             ExtractNonEmptyTiles();
             VeryfyInputTiles();
         }
 
         private void VeryfyInputTiles()
         {
-            if(topLeftTileCoords == null || bottonRightTileCoords == null)
+            if (topLeftTileCoords == null || bottomRightTileCoords == null)
             {
-                throw new System.Exception("WFC: Input Tilemap is empty");
+                throw new System.Exception("WFC: Input tIlemap is empty");
             }
-            int minX = bottonRightTileCoords.Value.x;
+            int minX = bottomRightTileCoords.Value.x;
             int maxX = topLeftTileCoords.Value.x;
-            int minY = bottonRightTileCoords.Value.y;
+            int minY = bottomRightTileCoords.Value.y;
             int maxY = topLeftTileCoords.Value.y;
 
             width = Math.Abs(maxX - minX) + 1;
             height = Math.Abs(maxY - minY) + 1;
 
             int tileCount = width * height;
-            if(StackOfTiles.Count != tileCount)
+            if (stackOftiles.Count != tileCount)
             {
                 throw new System.Exception("WFC: Tilemap has empty fields");
             }
-            if(stackOfTiles.Any(tile => tile.X > maxX || tile.X < minX || tile.Y > maxY || tile.Y < minY))
+            if (stackOftiles.Any(tile => tile.X > maxX || tile.X < minX || tile.Y > maxY || tile.Y < minY))
             {
                 throw new System.Exception("WFC: Tilemap image should be a filled rectangle");
-            } 
+            }
         }
-
-
-        /*
-         This next method will change a 1D array into a 2D array based on the width(which is inputTileMapBounds.size.x)
-
-        with array [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y] with a width of 5 will output the following:
-
-        [   a,b,c,d,e,
-            f,g,h,i,j,
-            k,l,m,n,o,
-            p,q,r,s,t,
-            u,v,w,x,y]
-         */
 
         private void ExtractNonEmptyTiles()
         {
-            for(int row = 0; row < this.inputTileMapBounds.size.y; row++)
+            for (int row = 0; row < inputTileMapBounds.size.y; row++)
             {
-                for(int col=0; col < this.inputTileMapBounds.size.x; col++)
+                for (int col = 0; col < inputTileMapBounds.size.x; col++)
                 {
                     int index = col + (row * inputTileMapBounds.size.x);
-                    TileBase tile = inputTilemapsTileArray[index];
-                    if(bottonRightTileCoords==null && tile!= null)
+
+                    TileBase tile = inputTilemapTilesArray[index];
+                    if (bottomRightTileCoords == null && tile != null)
                     {
-                        bottonRightTileCoords = new Vector2Int(col, row);
+                        bottomRightTileCoords = new Vector2Int(col, row);
                     }
-                    if(tile != null)
+                    if (tile != null)
                     {
-                        StackOfTiles.Enqueue(new TileContainer(tile, col,row));
+                        stackOftiles.Enqueue(new TileContainer(tile, col, row));
                         topLeftTileCoords = new Vector2Int(col, row);
                     }
                 }
