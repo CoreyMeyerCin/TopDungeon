557fb43fe631b175a50e534d932b8c40b8fbbf6c
Corey Meyer <72515852+CoreyMeyerCin@users.noreply.github.com>
2022-07-31 01:42:17 -0400

    Wfc cont (#132)
    * Took out useless comment for testing VS git commands
    
    * Added CoreSolver.cs and finished OutputGrid.cs
    
    Co-authored-by: DesrialBecker <beckerdl@miamioh.edu>

---
+++ Assets/Scripts/Core/CoreSolver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaveFunctionCollapse
+{
+
+    public class CoreSolver
+    {
+        //Steps:  1.Collapsing Cells
+                //2.Propagation
+                //2.Analyzing State
+
+        PatternManager patternManager;
+        OutputGrid outputGrid;
+    }
+}
+
