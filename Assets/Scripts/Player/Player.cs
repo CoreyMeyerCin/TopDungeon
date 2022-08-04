f6de63ad5bd7285c47c7aa7f7bb46efe4d56fc95
Corey Meyer <72515852+CoreyMeyerCin@users.noreply.github.com>
2022-08-04 03:19:04 -0400

    Building and city start (#141)
    * LootManager PickLootToDrop() needs a look
    
    * fix functionality bugs from moving code from enemycontroller to lootmanager
    
    * delete lootmanager ref to itself
    
    * Took useless stuff out of player, fixed dash logic for its cooldown & fixed boolean check for animation logic
    
    * Lots of building and city stuff added
    
    Co-authored-by: DesrialBecker <beckerdl@miamioh.edu>
    Co-authored-by: JeremyPlott <54282560+JeremyPlott@users.noreply.github.com>

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,6 +13,8 @@ public class Player : Fighter
     public PlayerAnimationController animationController;
     public PlayerActionController actionController;
     public Weapon weapon;
+    public Inventory inventory;
+
 
     public HealthService healthService;
     public PlayerSpriteService spriteService;
@@ -20,11 +22,6 @@ public class Player : Fighter
     public int skinId = 1;
     public double playerDirection;
     public int gold;
-
-    //public Projectile projectilePrefab; //holds the weapons prefab, might be able to do this in a better way to do this in the weapon
-    //public Projectile projectilePrefab;
-    //public float dashTime = 1f; //how long it takes to complete the full dash
-    public Vector3 dashStart, dashEnd;
 
 	public new void Awake()
 	{
@@ -63,81 +60,17 @@ public class Player : Fighter
     private void Update()
     {
         GetPlayerDirection();
-
-        //playerAnimator.GetAnimation();
-        //firePoint = this.transform;
-        //this.GetComponentInChildren<Weapon>().transform.localPosition += new Vector3(0.096f,-0.011f,0);
-
-        //projectilePrefab = GameManager.instance.player.transform.GetChild(0).gameObject.GetComponent<Projectile>();
     }
-    private void FixedUpdate()
-    {
-        float x = Input.GetAxisRaw("Horizontal"); // returns -1,1,or 0 depending if we are using a, d, or no input.
-        float y = Input.GetAxisRaw("Vertical"); // same thing with the y axis with w, s, or no input
-
-        //Reset MoveDelta
-        //UpdateMotor(new Vector3(x, y, 0));
-        currentPosition = transform.position;
-
-
-    }
-
     public void ChangeSkinId(int skinChange)
     {
         skinId = skinChange;
     }
-    public void SetDashLocationGoal(double playerDir)
-    {
-        switch (playerDir)
-        {
-            case 0:
-                dashEnd = new Vector3(currentPosition.x + (stats.speed / 5), currentPosition.y, currentPosition.z);
-                return;
-            case 0.5:
-                dashEnd = new Vector3(currentPosition.x + ((3 * stats.speed / 4) / 5), currentPosition.y - ((3 * stats.speed / 4) / 5), currentPosition.z);
-                return;
-            case 1:
-                dashEnd = new Vector3(currentPosition.x, currentPosition.y - ((3 * stats.speed / 4) / 5), currentPosition.z);
-                return;
-            case 1.5:
-                dashEnd = new Vector3(currentPosition.x - ((3 * stats.speed / 4) / 5), currentPosition.y - ((3 * stats.speed / 4) / 5), currentPosition.z);
-                return;
-            case 2:
-                dashEnd = new Vector3(currentPosition.x - (stats.speed / 10), currentPosition.y, currentPosition.z);
-                return;
-            case 2.5:
-                dashEnd = new Vector3(currentPosition.x - ((3 * stats.speed / 4) / 5), currentPosition.y + ((3 * stats.speed / 4) / 5), currentPosition.z);
-                return;
-            case 3:
-                dashEnd = new Vector3(currentPosition.x, currentPosition.y + stats.speed / 5, currentPosition.z);
-                return;
-            case 3.5:
-                dashEnd = new Vector3(currentPosition.x + ((3 * stats.speed / 4) / 5), currentPosition.y + ((3 * stats.speed / 4) / 10), currentPosition.z);
-                return;
-        }
-    }
 
     public void ChangeCurrentProjectile(Projectile proj, Weapon weap)
     {
-        Debug.Log("wtf happened to my projectiles");
-        weapon = weap;
-        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = weap.sprite;
-        transform.GetChild(0).GetComponent<Weapon>().weaponName=weap.weaponName;
         transform.GetChild(0).GetComponent<Weapon>().projectilePrefab = proj;
-        transform.GetChild(0).GetComponent<Weapon>().baseDamage = weap.baseDamage;
-        transform.GetChild(0).GetComponent<Weapon>().knockBack = weap.knockBack;
-        transform.GetChild(0).GetComponent<Weapon>().weaponType = weap.weaponType;
-        transform.GetChild(0).GetComponent<Weapon>().baseDamage = weap.baseDamage;
-        transform.GetChild(0).GetComponent<Weapon>().weaponType = weap.weaponType;
-        //transform.GetChild(0).GetComponent<Animator>().enabled = false;
-        transform.GetChild(0).GetComponent<Weapon>().holdPosition = weap.holdPosition;
-        transform.GetChild(0).GetComponent<Weapon>().animAttackType = weap.animAttackType;
-        transform.GetChild(0).GetComponent<Weapon>().animHoldType = weap.animHoldType;
-        //transform.GetChild(0).GetComponent<BoxCollider2D>().offset = weap.boxCollider.offset;
-        //transform.GetChild(0).GetComponent<BoxCollider2D>().size = weap.boxCollider.size;
-        PlayerAnimator.SetWeaponAnimationTree();
+        ChangeCurrentWeapon(weap);
     }
-
     public void ChangeCurrentWeapon(Weapon weap)
     {
         if (weap != null)
@@ -158,10 +91,10 @@ public class Player : Fighter
             //transform.GetChild(0).GetComponent<BoxCollider2D>().offset = weap.boxCollider.offset;
             //transform.GetChild(0).GetComponent<BoxCollider2D>().size = weap.boxCollider.size;
             PlayerAnimator.SetWeaponAnimationTree();
-
-            Debug.Log($"Equipped {weap.weaponName}");
+            //Debug.Log($"Equipped {weap.weaponName}");
         }
     }
+
 
     public void SwapSprite(int skinId)
     {
@@ -173,15 +106,6 @@ public class Player : Fighter
         stats.level++;
         stats.maxHitpoints = stats.IncreaseByFlatAmount(stats.maxHitpoints, 5f);
         stats.baseDamage = stats.IncreaseByFlatAmount(stats.baseDamage, 4f);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //Debug.Log($"Player has collided");
-        //if (collision.tag.Equals("Portal"))
-        //{
-        //    Debug.Log($"{collision}");
-        //}
     }
 
     public double GetPlayerDirection()
